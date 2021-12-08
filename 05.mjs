2b75e0bf773fc4cdea2fc50d6b64e8b40be7ff5b
Joseph Woolley <joe@calledtoconstruct.net>
2021-12-07 21:00:40 -0500

    Day 05 Part 02 Complete

--- 05.mjs
+++ 05.mjs
@@ -1,12 +1,13 @@
 
 function drawLine(board, from, to) {
-  const changeInX = (to.x - from.x);
-  const changeInY = (to.y - from.y);
-  const lengthOfLine = Math.floor(Math.sqrt(changeInX * changeInX + changeInY * changeInY));
-
+  const changeInX = Math.abs(to.x - from.x);
+  const changeInY = Math.abs(to.y - from.y);
+  const stepX = changeInX !== 0 ? (to.x - from.x) / Math.abs(to.x - from.x) : 0;
+  const stepY = changeInY !== 0 ? (to.y - from.y) / Math.abs(to.y - from.y) : 0;
+  const lengthOfLine = Math.max(changeInX, changeInY);
   for (let index = 0; index <= lengthOfLine; index++) {
-    const x = from.x + Math.round((changeInX / lengthOfLine) * index);
-    const y = from.y + Math.round((changeInY / lengthOfLine) * index);
+    const x = from.x + (stepX * index);
+    const y = from.y + (stepY * index);
     board[y][x] += 1;
   }
 }
@@ -19,7 +20,9 @@ function createRow(width) {
   return row;
 }
 
-function createBoard(width, height) {
+function createBoard(lines) {
+  let width = lines.reduce((accumulator, line) => Math.max(accumulator, line.from.x, line.to.x), 0) + 1;
+  let height = lines.reduce((accumulator, line) => Math.max(accumulator, line.from.y, line.to.y), 0) + 1;
   const board = [];
   while (height-- > 0) {
     const row = createRow(width);
@@ -45,13 +48,19 @@ function readInput(input) {
 function solvePartOne(input) {
   const lines = readInput(input)
     .filter(({ from, to }) => from.x === to.x || from.y === to.y);
-  const width = lines.reduce((accumulator, line) => accumulator = Math.max(accumulator, line.from.x, line.to.x), 0);
-  const height = lines.reduce((accumulator, line) => accumulator = Math.max(accumulator, line.from.y, line.to.y), 0);
-  const board = createBoard(width + 1, height + 1);
+  const board = createBoard(lines);
+  lines.forEach(({ from, to }) => drawLine(board, from, to));
+  return board.flat().filter(count => count > 1).length;
+}
+
+function solvePartTwo(input) {
+  const lines = readInput(input);
+  const board = createBoard(lines);
   lines.forEach(({ from, to }) => drawLine(board, from, to));
   return board.flat().filter(count => count > 1).length;
 }
 
 export {
-  solvePartOne
+  solvePartOne,
+  solvePartTwo
 }
--- test/05.spec.mjs
+++ test/05.spec.mjs
@@ -1,9 +1,11 @@
-import { solvePartOne } from '../05.mjs'
+import { solvePartOne, solvePartTwo } from '../05.mjs'
 import { runTest } from '../test.mjs'
 
 const description = 'when finding overlapping lines';
 
 [
   { description: `${description} for part one`, file: '05.sample.data', calculator: solvePartOne, result: 5 },
-  { description: `${description} for part one`, file: '05.data', calculator: solvePartOne, result: 6564 }
+  { description: `${description} for part one`, file: '05.data', calculator: solvePartOne, result: 6564 },
+  { description: `${description} for part two`, file: '05.sample.data', calculator: solvePartTwo, result: 12 },
+  { description: `${description} for part two`, file: '05.data', calculator: solvePartTwo, result: 19172 }
 ].forEach(runTest);
