18b02645df1b3dee53c9cdc87e77f771c5045e2d
Joseph Woolley <joe@calledtoconstruct.net>
2021-12-09 18:51:33 -0500

    Day 09 Part 02 Complete

--- 09.mjs
+++ 09.mjs
@@ -18,10 +18,10 @@ function load(input) {
 
 function getAdjacent(map, x, y) {
   const result = [];
-  if (x > 0) result.push(map.floor[y][x-1]);
-  if (x < map.width - 1) result.push(map.floor[y][x+1]);
-  if (y > 0) result.push(map.floor[y-1][x]);
-  if (y < map.height - 1) result.push(map.floor[y+1][x]);
+  if (x > 0) result.push(map.floor[y][x - 1]);
+  if (x < map.width - 1) result.push(map.floor[y][x + 1]);
+  if (y > 0) result.push(map.floor[y - 1][x]);
+  if (y < map.height - 1) result.push(map.floor[y + 1][x]);
   return result.reduce((accumulator, depth) => Math.min(accumulator, depth), Infinity);
 }
 
@@ -30,6 +30,41 @@ function solvePartOne(input) {
   return map.floor.reduce((mapAccumulator, line, y) => mapAccumulator + line.reduce((lineAccumulator, depth, x) => lineAccumulator + (getAdjacent(map, x, y) <= depth ? 0 : depth + 1), 0), 0);
 }
 
+function search(map, x, y) {
+  let basin = 0;
+  const searched = map.searched[y][x];
+  const plateau = map.floor[y][x] === 9;
+  if (!searched && !plateau) {
+    map.searched[y][x] = true;
+    if (x > 0) basin += search(map, x - 1, y);
+    if (y < map.height - 1) basin += search(map, x, y + 1);
+    if (y > 0) basin += search(map, x, y - 1);
+    if (x < map.width - 1) basin += search(map, x + 1, y);
+    basin++;
+  }
+  return basin;
+}
+
+function solvePartTwo(input) {
+  const map = load(input);
+  map.searched = map.floor.reduce((accumulator, line) => {
+    accumulator.push(Array.of(...line).fill(false))
+    return accumulator;
+  }, []);
+  const basins = [];
+  for (let y = 0; y < map.height; y++) {
+    for (let x = 0; x < map.width; x++) {
+      const basin = search(map, x, y);
+      if (basin > 0) {
+        basins.push(basin);
+      }
+    }
+  }
+  basins.sort((left, right) => right - left);
+  return basins[0] * basins[1] * basins[2];
+}
+
 export {
-  solvePartOne
+  solvePartOne,
+  solvePartTwo
 };
--- test/09.spec.mjs
+++ test/09.spec.mjs
@@ -1,9 +1,11 @@
-import { solvePartOne } from '../09.mjs'
+import { solvePartOne, solvePartTwo } from '../09.mjs'
 import { runTest } from '../test.mjs'
 
 const description = 'when deducing the segments';
 
 [
   { description: `${description} for part one`, file: '09.sample.data', calculator: solvePartOne, result: 15 },
-  { description: `${description} for part one`, file: '09.data', calculator: solvePartOne, result: 631 }
+  { description: `${description} for part one`, file: '09.data', calculator: solvePartOne, result: 631 },
+  { description: `${description} for part two`, file: '09.sample.data', calculator: solvePartTwo, result: 1134 },
+  { description: `${description} for part two`, file: '09.data', calculator: solvePartTwo, result: 821560 }
 ].forEach(runTest);
